5f3df6de612d9286b9162300ec4ea79097fd7d0b
Toffee <toffee24@foxmail.com>
2018-02-24 15:40:20 +0800

    add csrf

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf.csrf import CSRFProtect
 
 from app.admin import admin as admin_blueprint
 from app.home import home as home_blueprint
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@127.0.0.1:3306/movie?charset=utf8'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = '7e9a03d9d42c4e15a847d8fb04e49847'
+# app.config['WTF_CSRF_SECRET_KEY'] = '7e9a03d9d42c4e15a847d8fb04e49847'
 
 app = Flask(__name__)
 app.debug = True
-# db = SQLAlchemy
+# db = SQLAlchemy(app)
 
 # 注册蓝图
 app.register_blueprint(home_blueprint)
